544287018c273adb01cb8093b798f259c1879d9e
LeoDisruptual <leo@disruptual.com>
2021-04-16 17:38:28 +0200

    fix Signup

--- navigation/SwitchNavigator.jsx
+++ navigation/SwitchNavigator.jsx
@@ -1,6 +1,7 @@
 import { createSwitchNavigator, createAppContainer } from 'react-navigation';
 import Login from '../api/Login';
 import Profile from '../api/Profile';
+import Signup from '../api/Signup';
 import Firebase from '../config/firbaseConfig';
 
 let SwitchNavigator;
@@ -9,6 +10,9 @@ if (Firebase.auth().currentUser) {
     {
       Login: {
         screen: Login,
+      },
+      Signup: {
+        screen: Signup,
       },
       Profile: {
         screen: Profile,
@@ -23,6 +27,9 @@ if (Firebase.auth().currentUser) {
     {
       Login: {
         screen: Login,
+      },
+      Signup: {
+        screen: Signup,
       },
       Profile: {
         screen: Profile,
