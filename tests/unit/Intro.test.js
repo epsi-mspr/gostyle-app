60b6769091afcb56de5a271f73ec2e1f3fd2effa
Wawa27 <32011804+Wawa27@users.noreply.github.com>
2021-04-16 23:55:33 +0200

    feat: add navigation (#12)

---
+++ tests/unit/Intro.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react-native';
+import App from '../../App';
+
+test('page should have 3 differents pages (Home, Account, Promotions)', async () => {
+  const { findByText } = render(<App />);
+  const homePage = await findByText('Account');
+  const promotionsPage = await findByText('Promotions');
+  const accountPage = await findByText('Account');
+
+  expect(homePage)
+    .toBeTruthy();
+
+  expect(promotionsPage)
+    .toBeTruthy();
+
+  expect(accountPage)
+    .toBeTruthy();
+});
+
+test('clicking the account button should redirect to the account page', async () => {
+  const { findByText } = render(<App />);
+  const homePage = await findByText('Account');
+
+  fireEvent(homePage, 'click');
+
+  const accountScreen = await findByText('AccountScreen');
+  expect(accountScreen)
+    .toBeTruthy();
+});
+
+test('clicking the home button should redirect to the home page', async () => {
+  const { findByText } = render(<App />);
+  const homePage = await findByText('Home');
+
+  fireEvent(homePage, 'click');
+
+  const homeScreen = await findByText('HomeScreen');
+  expect(homeScreen)
+    .toBeTruthy();
+});
+
+test('clicking the promotions button should redirect to the promotions page', async () => {
+  const { findByText } = render(<App />);
+  const promotionsPage = await findByText('Promotions');
+
+  fireEvent(promotionsPage, 'click');
+
+  const promotionsScreen = await findByText('La liste des promotions:');
+  expect(promotionsScreen)
+    .toBeTruthy();
+});
