4b937fbf6abbc37ee9238817e87b6f95712f6497
Wawa27 <32011804+Wawa27@users.noreply.github.com>
2021-04-16 23:55:33 +0200

    feat: add navigation (#12)

---
+++ App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AccountScreen from './components/AccountScreen';
+import HomeScreen from './components/HomeScreen';
+import PromotionScreen from './components/PromotionScreen';
+
+export default function App() {
+  const Tab = createBottomTabNavigator();
+
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: (icon) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = icon.focused
+                ? 'home'
+                : 'home-outline';
+            } else if (route.name === 'Account') {
+              iconName = icon.focused ? 'person' : 'person-outline';
+            } else if (route.name === 'Promotions') {
+              iconName = icon.focused ? 'list' : 'list-outline';
+            }
+            const {
+              size,
+              color
+            } = icon;
+            return <Ionicons name={iconName} size={size} color={color} />;
+          }
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray'
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Promotions" component={PromotionScreen} />
+        <Tab.Screen name="Account" component={AccountScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
