48d242d5332a69c102d0b842bee69db13ff13aa9
Wawa27 <32011804+Wawa27@users.noreply.github.com>
2021-04-18 15:33:58 +0200

    feat: add login (#14)

---
+++ components/Card.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import * as PropTypes from "prop-types";
+
+const styles = StyleSheet.create({
+  card: {
+    borderRadius: 6,
+    elevation: 3,
+    backgroundColor: "#fff",
+    shadowOffset: {
+      width: 1,
+      height: 1
+    },
+    shadowColor: "#333",
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    marginHorizontal: 4,
+    marginVertical: 6
+  },
+  cardContent: {
+    marginHorizontal: 100,
+    marginVertical: 30
+  }
+});
+
+export default function Card(props) {
+  const { children } = props;
+  return (
+    <View style={styles.card}>
+      <View style={styles.cardContent}>{children}</View>
+    </View>
+  );
+}
+
+Card.propTypes = {
+  children: PropTypes.node.isRequired
+};
---
+++ navigation/SwitchNavigator.js
@@ -0,0 +1,36 @@
+import { createSwitchNavigator, createAppContainer } from 'react-navigation';
+import Login from '../api/Login';
+import Profile from '../api/Profile';
+import Firebase from '../config/firebaseConfig';
+
+let SwitchNavigator;
+if (Firebase.auth().currentUser) {
+  SwitchNavigator = createSwitchNavigator(
+    {
+      Login: {
+        screen: Login
+      },
+      Profile: {
+        screen: Profile
+      }
+    },
+    {
+      initialRouteName: 'Profile'
+    }
+  );
+} else {
+  SwitchNavigator = createSwitchNavigator(
+    {
+      Login: {
+        screen: Login
+      },
+      Profile: {
+        screen: Profile
+      }
+    },
+    {
+      initialRouteName: 'Login'
+    }
+  );
+}
+export default createAppContainer(SwitchNavigator);
